002c94dd63e0111e12ad0ea42ecef9d85883bf47
Henri Ruoho <henri.ruoho@reaktor.com>
2019-05-04 13:06:52 +0300

    Fix landing page urls

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container-fluid">
     <div class="jumbotron">
-      <h1 class="display-4">Our biases and fallacies</h1>
+      <h1 class="display-4">Fallacies and biases</h1>
       <p class="lead">
         A non-profit, bootleg version of
-        <a href="yourbias.is">yourbias.is</a> and
-        <a href="yourfallacy.is">yourfallacy.is</a>, aiming to serve the information in a minimalistic form.
+        <a href="https://yourbias.is">yourbias.is</a> and
+        <a href="https://yourfallacy.is">yourfallacy.is</a>, aiming to serve the information in a minimalistic form.
       </p>
       <hr class="my-4">
       <p>All credit belongs to the original authors.</p>
